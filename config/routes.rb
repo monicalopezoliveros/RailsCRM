49486480a628c624f285104fbadb2b74d3d91840
mlopez <monicalopezoliveros@gmail.com>
2024-11-08 12:36:54 -0600

    Add customer partial view to display customer info with image, use image_tag helper, and add fallback image for customers without photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  # Routes for the customers controller.
+  resources :customers, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
